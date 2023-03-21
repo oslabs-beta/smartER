3c49ed79a493be7072e1282936b153c2ce693e87
Joyce Kwak <joycekwak@Joyces-MacBook-Air.local>
2023-03-21 12:30:06 -0700

    created additional components for our homepage and created state
    Co-authored-by: joyxek <joycekwak73@gmail.com>
    Co-authored-by: melissamcl <mcl.melissa.m@gmail.com>
    Co-authored-by: b-v-u <briandoanvu@gmail.com>
    Co-authored-by: n8ngo <nathanngo8796@gmail.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,27 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { LoginContext } from './Context';
+import { LoginContext, HomepageContext } from './Context';
 import Login from './components/Login';
+import Homepage from './components/Homepage';
 import Signup from './components/Signup';
 
 const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [home, setHome] = useState('');
+  const [queryString, setQueryString] = useState('');
+  const [history, setHistory] = useState([
+    { created_at: 'hello', query: 'world' },
+  ]);
+  const [uri, setUri] = useState('');
+  const [dbCredentials, setDbCredentials] = useState({
+    host: '',
+    port: 3000,
+    dbUsername: '',
+    dbPassword: '',
+    database: '',
+  });
+
   return (
     <LoginContext.Provider
       value={{
@@ -16,10 +31,26 @@ const App = () => {
         setPassword,
       }}
     >
-      <Routes>
-        <Route path="/" element={<Login />} />;
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <HomepageContext.Provider
+        value={{
+          home,
+          setHome,
+          queryString,
+          setQueryString,
+          history,
+          setHistory,
+          uri,
+          setUri,
+          dbCredentials,
+          setDbCredentials,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Login />} />;
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </HomepageContext.Provider>
     </LoginContext.Provider>
   );
 };
--- client/Context.tsx
+++ client/Context.tsx
@@ -1,10 +1,12 @@
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext, Dispatch, SetStateAction } from 'react';
+import { StringLiteral } from 'typescript';
 
+// Login context
 type LoginContextType = {
   email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  setEmail: Dispatch<SetStateAction<string>>;
   password: string;
-  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  setPassword: Dispatch<SetStateAction<string>>;
 };
 
 const defaultState = {
@@ -18,4 +20,59 @@ export const LoginContext = createContext<LoginContextType | undefined>(
   defaultState
 );
 
-//TODO: add a Homepage Context
+//Homepage Context
+type HomepageContextType = {
+  home: string;
+  setHome: Dispatch<SetStateAction<string>>;
+  queryString: string;
+  setQueryString: Dispatch<SetStateAction<string>>;
+  history: historyType[];
+  setHistory: Dispatch<SetStateAction<historyType[]>>;
+  uri: string;
+  setUri: Dispatch<SetStateAction<string>>;
+  dbCredentials: dbCredentialsType;
+  setDbCredentials: Dispatch<SetStateAction<dbCredentialsType>>;
+};
+
+const defaultHomeState = {
+  home: 'HOMEPAGE',
+  setHome: () => {},
+  queryString: '',
+  setQueryString: () => {},
+  history: [],
+  setHistory: () => {},
+  uri: '',
+  setUri: () => {},
+  dbCredentials: {},
+  setDBCredentials: () => {},
+};
+
+export const HomepageContext = createContext<HomepageContextType | undefined>(
+  defaultHomeState
+);
+
+//History [{created_at: string, query: string}]
+
+//TYPES
+interface historyType {
+  created_at: string;
+  query: string;
+}
+
+interface dbCredentialsType {
+  host: string;
+  port: number;
+  dbUsername: string;
+  dbPassword: string;
+  database: string;
+}
+
+//TODO: Create state for Nodes
+/*
+//State to manage:
+[]Nodes
+[x]Query
+[x]History
+[x]Settings
+
+*/
