99a92bc72a7e9befd341786d28a5b4d2068def3a
n8ngo <nathanngo8796@gmail.com>
2023-03-20 17:33:45 -0700

    Initial progress UseContext
    Co-authored-by: joyxek <joycekwak73@gmail.com>
    Co-authored-by: n8ngo <nathanngo8796@gmail.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
+import {GlobalContext} from './GlobalContext';
 import Login from './components/Login';
 
 export const App = () => {
   return (
-    <>
+    <GlobalContext.Provider
+      value={{
+        email: 'hello@world.com',
+        setEmail: () => console.log('email was set'),
+      }}
+    >
       <h1>Auto Sql</h1>
       <Login />
-    </>
+    </GlobalContext.Provider>
   );
 };
+
+// const contextValue = useMemo(
+//   () => ({
+//     email,
+//     setEmail,
+//   }),
+//   [email, setEmail]
+// );
--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,13 +1,19 @@
-import React, {FC} from 'react';
+import React, {FC, useContext} from 'react';
 import {useNavigate} from 'react-router-dom';
+import {GlobalContext} from '../GlobalContext';
 
 const Heading = ({title}: {title: string}) => <h2>{title}</h2>;
 
 const Login: React.FC<{}> = () => {
+  const context = useContext(GlobalContext);
+
   return (
     <>
       <div>HELLO WORLD</div>
       <Heading title="Hello" />
+      <div>{context.email}</div>
+
+      <button onClick={context.setEmail}>Click me</button>
     </>
   );
 };
