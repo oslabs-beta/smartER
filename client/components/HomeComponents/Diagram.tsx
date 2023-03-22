1f5d70d2bfd539dcb9b1d724d886e111f96210b9
n8ngo <nathanngo8796@gmail.com>
2023-03-22 15:05:54 -0700

    merged components, resize horizontal Co-authored-by: n8ngo <nathanngo8796@gmail.com> Co-authored-by: joyxek <joycekwak73@gmail.com>

---
+++ client/components/HomeComponents/Diagram.tsx
@@ -0,0 +1,7 @@
+import React, {FC, useContext, useState} from 'react';
+
+const Diagram: React.FC<{}> = () => {
+  return <div className="diagram-box">REACT FLOW DIAGRAM HERE</div>;
+};
+
+export default Diagram;
--- client/components/Homepage.tsx
+++ client/components/Homepage.tsx
@@ -1,33 +1,76 @@
 import React, {FC, useState, useContext, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
 import Diagram from './HomeComponents/Diagram';
 import Header from './HomeComponents/Header';
 import InputContainer from './HomeComponents/InputContainer';
 import QueryResults from './HomeComponents/QueryResults';
-import {HomepageContext} from '../Context';
+import Split from 'react-split';
 
-const Homepage: React.FC<{}> = () => {
-  const {home, setHome} = useContext(HomepageContext)!;
-  const {queryString, setQueryString} = useContext(HomepageContext)!;
-  const {history, setHistory} = useContext(HomepageContext)!;
-  const {dbCredentials, setDBCredentials} = useContext(HomepageContext)!;
-
-  // useEffect(() => {
-  //   setHome('HELLO WORLD');
-  //   setQueryString('setting query string');
-  //   console.log('history: ', history);
-  //   console.log('dbCredentials: ', dbCredentials);
-  //   setHistory([{created_at: 'hello', query: 'word'}]);
-  //   console.log('history: ', history);
-  // }, []);
-
+const TopComp = () => {
   return (
     <div>
-      <Header />
-      <QueryResults />
-      <div></div>
+      <Split className="flex" sizes={[30, 70]} minSize={[270, 40]}>
+        <InputContainer />
+        <Diagram />
+      </Split>
     </div>
   );
 };
 
+const LowerComp = () => {
+  return (
+    <div>
+      <QueryResults />
+    </div>
+  );
+};
+
+const Homepage: React.FC<{}> = () => {
+  return (
+    <>
+      <Header />
+      <div className="main-container">
+        <Split direction="vertical" minSize={[600, 200]} gutterSize={5}>
+          <TopComp />
+          <LowerComp />
+        </Split>
+      </div>
+    </>
+  );
+};
+
 export default Homepage;
+
+{
+  /* <Split
+      className="flex2"
+      direction="vertical"
+      sizes={[30, 70]}
+      style={{height: 'calc(100vh-4rem'}}
+    >
+      <Split className="flex" sizes={[20, 80]} minSize={[10, 40]}>
+        <div>Query</div>
+        <div>Diagram</div>
+      </Split>
+      <div>Results</div>
+    </Split>
+    
+    */
+}
+
+//vertial split works, but horizontal split does not
+/*
+  return (
+    <Split
+      direction="vertical"
+      sizes={[70, 30]}
+      style={{height: 'calc(100vh-4rem'}}
+    >
+      <Split className="flex" sizes={[30, 70]} minSize={[10, 40]}>
+        <InputContainer />
+        <Diagram />
+      </Split>
+      <QueryResults />
+    </Split>
+  );
+
+*/
