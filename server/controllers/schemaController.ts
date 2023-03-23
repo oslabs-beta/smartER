7f9fc85889f2adf81f7f6b9e1adfb637da64a6dd
Brian Vu <briandoanvu@gmail.com>
2023-03-22 23:17:25 -0700

    Updated single query and helper ts file to be dynamic

--- server/controllers/schemaController.ts
+++ server/controllers/schemaController.ts
@@ -1,9 +1,5 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
-import {
-  currentColumnHasForeignKey,
-  currentColumnHasPrimaryKey,
-  getAllRelationShips,
-} from '../helper/constraintType';
+import { getAllQuery } from '../helper/getAllQuery';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -37,31 +33,11 @@ const schemaController: schemaControllers = {
       const pg = new Pool(programmaticCredentials || envCredentials);
 
       // Get all relationships between all tables
-      const getRelationshipsTableNamesColumnNamesDataType = await pg.query(
-        `select
-        c.table_name,
-        c.column_name,
-        max(c.data_type) as data_type,
-        max(case when tc.constraint_type = 'PRIMARY KEY' then 1 else 0 end) AS primary_key_exists,
-        max(cc.table_name) as table_origin,
-        max(cc.column_name) as table_column
-
-       from information_schema.key_column_usage kc
-
-       right join information_schema.columns c
-       on c.table_name = kc.table_name and c.column_name = kc.column_name
-
-       left join information_schema.table_constraints tc
-       on kc.table_name = tc.table_name and kc.table_schema = tc.table_schema and kc.constraint_name = tc.constraint_name
-
-       left join information_schema.constraint_column_usage cc
-       on cc.constraint_name = kc.constraint_name and tc.constraint_type = 'FOREIGN KEY'
-
-       where c.table_schema = 'public' and is_updatable = 'YES'
-
-       group by c.table_name, c.column_name
-       order by c.table_name;`
+      const currentSchema = await pg.query(
+        `select current_schema from current_schema`
       );
+      // Get Relationships, Tables names, Column names, Data types
+      const RTNCND = await pg.query(getAllQuery(currentSchema));
 
       // Initialize array to hold returned data
       const erDiagram = [];
@@ -76,18 +52,12 @@ const schemaController: schemaControllers = {
         columns: [],
       };
       // Assign prev table name and tableObj.table_name to be the first table name from the query
-      let prevTableName =
-        getRelationshipsTableNamesColumnNamesDataType.rows[0].table_name;
-      tableObj.table_name =
-        getRelationshipsTableNamesColumnNamesDataType.rows[0].table_name;
+      let prevTableName = RTNCND.rows[0].table_name;
+      tableObj.table_name = RTNCND.rows[0].table_name;
       // Iterate through array of all table names, columns, and data types
-      for (
-        let i = 0;
-        i < getRelationshipsTableNamesColumnNamesDataType.rows.length;
-        i++
-      ) {
+      for (let i = 0; i < RTNCND.rows.length; i++) {
         // current represents each object in the array
-        const current = getRelationshipsTableNamesColumnNamesDataType.rows[i];
+        const current = RTNCND.rows[i];
         //column object type
         const column: Record<string, any> = {};
         // Check to see if the prev table name does not match the current table name
@@ -107,21 +77,7 @@ const schemaController: schemaControllers = {
         if (current.table_origin)
           column.linkedTable =
             current.table_origin + '.' + current.table_column;
-        // Adds relationships to columns that have a constraint type, splice choice out if we find a constraint
 
-        // prettier-ignore
-        //   {
-        //   if (currentColumnHasPrimaryKey(tableRelationship, current.table_name, current.column_name)) {
-        //     column.primaryKey = true;
-
-        //     break;
-        //   } else if (currentColumnHasForeignKey(tableRelationship, current.table_name, current.column_name)) {
-        //     column.linkedTable = tableRelationship.table_origin + '.' + tableRelationship.table_column;
-
-        //     break;
-        //   }
-        //   }
-        // }
         // Push the complete column object into columns array
         tableObj.columns.push(column);
       }
