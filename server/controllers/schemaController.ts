3008e064ada60acc810759521128b3b4fb688bfe
melissamcl <mcl.melissa.m@gmail.com>
2023-03-23 22:15:20 -0400

    query changes and some minor edits

--- server/controllers/schemaController.ts
+++ server/controllers/schemaController.ts
@@ -5,14 +5,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface schemaControllers {
+  connectDb: RequestHandler;
   getSchemaPostgreSQL: RequestHandler;
-  // getQueryResults: RequestHandler;
+  getQueryResults: RequestHandler;
+  getQueryPerformance: RequestHandler;
 }
 //
 const schemaController: schemaControllers = {
-  getSchemaPostgreSQL: async (req, res, next) => {
-    try {
-      /*
+  connectDb: async (req, res, next) => {
+    /*
       FE will always send URI so below logic is not needed
 
       const { programmatic, pg_url } = req.body;
@@ -35,13 +36,26 @@ const schemaController: schemaControllers = {
       }
       */
 
+    try {
       // we should probably refactor this to get URI from db basaed on user/JWT
       const { pg_url } = req.body;
       const PG_URL = pg_url || process.env.PG_URL_STARWARS;
       var envCredentials: any = { connectionString: PG_URL };
       // const pg = new Pool(programmaticCredentials || envCredentials);
-      const pg = new Pool(envCredentials);
+      res.locals.pg = new Pool(envCredentials);
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error in schemaController.connectDb ${error}`,
+        status: 400,
+        message: { error },
+      });
+    }
+  },
 
+  getSchemaPostgreSQL: async (req, res, next) => {
+    try {
+      const pg = res.locals.pg;
       // Get all relationships between all tables
       // Identify the current schema name for use in full schema query
       const currentSchema = await pg.query(
@@ -50,7 +64,6 @@ const schemaController: schemaControllers = {
 
       // Get Relationships, Tables names, Column names, Data types
       const schema = await pg.query(getAllQuery(currentSchema));
-      console.log('schema results', schema);
 
       // Table type
       interface table {
@@ -65,7 +78,7 @@ const schemaController: schemaControllers = {
       };
       // Assign prev table name and tableObj.table_name to be the first table name from the query
       let prevTableName = schema.rows[0].table_name;
-      tableObj.table_name = schema.rows[0].table_name;
+      tableObj.table_name = prevTableName;
       // Iterate through array of all table names, columns, and data types
       for (let i = 0; i < schema.rows.length; i++) {
         // current represents each object in the array
@@ -87,7 +100,7 @@ const schemaController: schemaControllers = {
         // create column_name : data_type key value pair on column
         column[current.column_name] = current.data_type;
         // Check if primary key exists and if foreign key exists
-        if (current.primary_key_exists) column.primary_key = true;
+        if (current.is_primary_key) column.primary_key = true;
         if (current.table_origin)
           column.linkedTable =
             current.table_origin + '.' + current.table_column;
@@ -95,7 +108,9 @@ const schemaController: schemaControllers = {
         // Push the complete column object into columns array
         tableObj.columns.push(column);
       }
-      return res.json(erDiagram);
+      // return res.json(erDiagram);
+      res.locals.erDiagram = erDiagram;
+      return next();
     } catch (error) {
       return next({
         log: `Error in schemaController.getSchema ${error}`,
@@ -104,40 +119,45 @@ const schemaController: schemaControllers = {
       });
     }
   },
-  // getQueryResults: async (req, res, next) => {
-  //   try {
-  //     // FE Provides whether or not user is logging in programmatically
-  //     // If programmatically logging in, create a credentials object to
-  //     // create a new connection, else we assign it the provided pg_url
-  //     // if that is not provided, we assign it the starwars pg_url
-  //     const { programmatic, pg_url, queryString } = req.body;
-  //     if (programmatic) {
-  //       var { host, port, dbUsername, dbPassword, database } = req.body;
-  //       var programmaticCredentials: any = {
-  //         host,
-  //         port,
-  //         user: dbUsername,
-  //         password: dbPassword,
-  //         database,
-  //       };
-  //     } else {
-  //       const PG_URL = pg_url || process.env.PG_URL_STARWARS;
-  //       var envCredentials: any = { connectionString: PG_URL };
-  //     }
-  //     const pg = new Pool(programmaticCredentials || envCredentials);
-  //     const pg = new Pool(programmaticCredentials || envCredentials);
-  //     // Make a query based on the passed in queryString
-  //     const getQuery = await pg.query(queryString);
-  //     // Return query to FE
-  //     res.json(getQuery.rows);
-  //   } catch (error) {
-  //     return next({
-  //       log: `Error in schemaController.getQueryResults ${error}`,
-  //       status: 400,
-  //       message: { error },
-  //     });
-  //   }
-  // },
+  getQueryResults: async (req, res, next) => {
+    try {
+      const { queryString } = req.body;
+      const pg = res.locals.pg;
+      // Make a query based on the passed in queryString
+      const getQuery = await pg.query(queryString);
+      // Return query to FE
+      res.json(getQuery.rows);
+    } catch (error) {
+      return next({
+        log: `Error in schemaController.getQueryResults ${error}`,
+        status: 400,
+        message: { error },
+      });
+    }
+  },
+  getQueryPerformance: async (req, res, next) => {
+    try {
+      const pg = res.locals.pg;
+      const currentSchema = await pg.query(
+        `SELECT current_schema FROM current_schema`
+      );
+
+      // Get Relationships, Tables names, Column names, Data types
+      const schema = await pg.query(
+        `EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) ${getAllQuery(
+          currentSchema
+        )}`
+      );
+
+      res.json(schema.rows[0]['QUERY PLAN'][0]);
+    } catch (error) {
+      return next({
+        log: `Error in schemaController.getQueryPerformance ${error}`,
+        status: 400,
+        message: { error },
+      });
+    }
+  },
 };
 
 export default schemaController;
--- server/helper/getAllQuery.ts
+++ server/helper/getAllQuery.ts
@@ -1,25 +1,31 @@
 export const getAllQuery = (currentSchema: any) => {
-  return `select
-  c.table_name,
-  c.column_name,
-  max(c.data_type) as data_type,
-  max(case when tc.constraint_type = 'PRIMARY KEY' then 1 else 0 end) AS primary_key_exists,
-  max(cc.table_name) as table_origin,
-  max(cc.column_name) as table_column
+  return `SELECT * FROM (
+    SELECT DISTINCT ON (c.table_name, c.column_name)
+        c.table_name, 
+        c.column_name, 
+        c.data_type,
+        c. ordinal_position,
+        max(case when tc.constraint_type = 'PRIMARY KEY' then 1 else 0 end) OVER(PARTITION BY c.table_name, c.column_name) AS is_primary_key,
+        cc.table_name as table_origin,
+        cc.column_name as table_column
 
- from information_schema.key_column_usage kc
+    FROM information_schema.key_column_usage kc
 
- right join information_schema.columns c
- on c.table_name = kc.table_name and c.column_name = kc.column_name
+    INNER JOIN information_schema.table_constraints tc
+    ON kc.table_name = tc.table_name AND kc.table_schema = tc.table_schema AND kc.constraint_name = tc.constraint_name 
 
- left join information_schema.table_constraints tc
- on kc.table_name = tc.table_name and kc.table_schema = tc.table_schema and kc.constraint_name = tc.constraint_name
+    LEFT JOIN information_schema.constraint_column_usage cc
+    ON cc.constraint_name = kc.constraint_name AND tc.constraint_type = 'FOREIGN KEY'
 
- left join information_schema.constraint_column_usage cc
- on cc.constraint_name = kc.constraint_name and tc.constraint_type = 'FOREIGN KEY'
+    RIGHT JOIN information_schema.columns c
+    ON c.table_name = kc.table_name AND c.column_name = kc.column_name
 
- where c.table_schema = '${currentSchema.rows[0].current_schema}' and is_updatable = 'YES'
+    WHERE c.table_schema = '${currentSchema.rows[0].current_schema}' AND is_updatable = 'YES'
 
- group by c.table_name, c.column_name
- order by c.table_name, primary_key_exists desc, table_origin desc;`;
+    ORDER BY c.table_name, c.column_name, is_primary_key desc, table_origin) subquery
+  
+  ORDER BY table_name, ordinal_position;`;
 };
+
+// Planning Time: 7.641 ms
+// Execution Time: 101.963 ms
