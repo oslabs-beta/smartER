2cd85f8cb41d792af62a966d44f9a0beba46e0ef
Joyce Kwak <joycekwak@Joyces-MacBook-Air.local>
2023-03-24 00:13:20 -0700

    rendered table for the query results using dummy data
    Co-authored-by: joyxek <joycekwak73@gmail.com>
    Co-authored-by: n8ngo <nathanngo8796@gmail.com>
    Co-authored-by: b-v-u <briandoanvu@gmail.com>

--- client/components/HomeComponents/InputContainerComponents/QueryInput.tsx
+++ client/components/HomeComponents/InputContainerComponents/QueryInput.tsx
@@ -1,14 +1,33 @@
 import React, { FC, useState, useContext, useEffect } from 'react';
 import { HomepageContext } from '../../../Context';
+import { historyType } from '../../../Context';
 
 const QueryInput: React.FC<{}> = () => {
   const { queryString, setQueryString } = useContext(HomepageContext)!;
+  const { history, setHistory } = useContext(HomepageContext)!;
+  const { submit, setSubmit } = useContext(HomepageContext)!;
 
   //TODO: Add logic for live rendering nodes n stuff
 
+  // on submit query result and er diagram will render
   const handleSubmit = (e: any): void => {
-    e.preventDefault;
-    console.log('QUERY HANDLE SUBMIT ', queryString);
+    e.preventDefault();
+    try {
+      // POST request to database with queryString
+
+      // GET request to database with object from the Query
+      const api = async () => {
+        const data = await fetch('https://localhost:9001/api/getQueryResults', {
+          method: 'GET',
+        });
+        const jsonData = await data.json();
+      };
+      api();
+      // response from GET should be sent to the Query Result component
+      console.log('QUERY HANDLE SUBMIT ', queryString);
+    } catch (err) {
+      console.log('error in get');
+    }
   };
 
   return (
@@ -18,6 +37,7 @@ const QueryInput: React.FC<{}> = () => {
         required
         placeholder="type your query"
         onChange={(e) => setQueryString(e.target.value)}
+        value={queryString}
       ></textarea>
       <button
         type="submit"
