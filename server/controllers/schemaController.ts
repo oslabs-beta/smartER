4818cddd4991954f12cdc1db8d3f94ca74cc4490
Brian Vu <briandoanvu@gmail.com>
2023-03-24 22:28:33 -0700

    Add new Object layout for FE Co-authored-by: joyxek <joycekwak73@gmail.com> Co-authored-by: melissamcl <mcl.melissa.m@gmail.com> Co-authored-by: n8ngo <nathanngo8796@gmail.com>

--- server/controllers/schemaController.ts
+++ server/controllers/schemaController.ts
@@ -65,55 +65,57 @@ const schemaController: schemaControllers = {
       const schema = await pg.query(getAllQuery(currentSchema));
 
       // Table type
-      interface table {
-        table_name: string;
-        columns: any[];
-      }
-      // Initialize array to hold returned data
-      const erDiagram = [];
-      let tableObj: table = {
-        table_name: '',
-        columns: [],
+
+      const masterObj = {
+        people: {
+          someColumnName: {
+            // EVERY DATA WE NEED
+          },
+        },
+        films: {},
       };
+      // Initialize array to hold returned data
+      const erDiagram: Record<string, typeof tableObj> = {};
+      let tableObj: Record<string, any> = {};
       // Assign prev table name and tableObj.table_name to be the first table name from the query
       let prevTableName = schema.rows[0].table_name;
-      tableObj.table_name = prevTableName;
+
       // Iterate through array of all table names, columns, and data types
       for (let i = 0; i < schema.rows.length; i++) {
         // current represents each object in the array
         const current = schema.rows[i];
         //column object type and declaration
-        const column: Record<string, any> = {};
 
         // Check to see if the prev table name does not match the current table name
         // if it doesn't match, we know we are in a different table
         // push a deep copy of the tableObj, assign the current table_name to the tableObj.table_name
         if (prevTableName !== current.table_name) {
-          erDiagram.push({ ...tableObj });
-          tableObj.table_name = current.table_name;
-          tableObj.columns = [];
+          erDiagram[prevTableName] = { ...tableObj };
+          tableObj = {};
         }
         // Update prevTableName so we can keep track of when we enter a new table
         prevTableName = current.table_name;
-
+        tableObj[current.column_name] = {};
         // Assign table name and column name
-        column.table_name = current.table_name;
-        column.column_name = current.column_name;
+        tableObj[current.column_name].table_name = current.table_name;
+        tableObj[current.column_name].column_name = current.column_name;
         // Assign data type
-        if (current.data_type === 'integer') column.data_type = 'int';
+        if (current.data_type === 'integer')
+          tableObj[current.column_name].data_type = 'int';
         else if (current.data_type === 'character varying')
-          column.data_type = 'varchar';
-        else column.data_type = current.data_type;
+          tableObj[current.column_name].data_type = 'varchar';
+        else tableObj[current.column_name].data_type = current.data_type;
         // Add relationships and constraints if there are any
-        if (current.primary_key_exists) column.primary_key = true;
+        if (current.primary_key_exists)
+          tableObj[current.column_name].primary_key = true;
         if (current.table_origin) {
-          column.foreign_key = true;
-          column.linkedTable = current.table_origin;
-          column.linkedTableColumn = current.table_column;
+          tableObj[current.column_name].foreign_key = true;
+          tableObj[current.column_name].linkedTable = current.table_origin;
+          tableObj[current.column_name].linkedTableColumn =
+            current.table_column;
         }
 
         // Push the complete column object into columns array
-        tableObj.columns.push(column);
       }
       // return res.json(erDiagram);
       res.locals.erDiagram = erDiagram;
