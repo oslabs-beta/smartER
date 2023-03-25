dfc2916d615acf58f826de10800afac24ba9a391
Brian Vu <briandoanvu@gmail.com>
2023-03-24 22:01:23 -0700

    Add new Object layout for FE Co-authored-by: joyxek <joycekwak73@gmail.com> Co-authored-by: melissamcl <mcl.melissa.m@gmail.com> Co-authored-by: n8ngo <nathanngo8796@gmail.com>

--- client/components/SampleData.tsx
+++ client/components/SampleData.tsx
@@ -508,128 +508,269 @@ function parseDataEdges(data: any): any {
 }
 
 let str = `
-SELECT p.*,    ," ",      s.name        AS species,  h.name AS homeworld
+SELECT p.*, s.name AS species,  h.name AS homeworld
 FROM people p
 LEFT JOIN species s ON p.species_id = s._id
 LEFT JOIN planets h ON p.homeworld_id = h._id`;
-
+/**
+ *
+ALTER TABLE species
+RENAME COLUMN eye_colors TO "eye colors"
+select "eye colors" from species
+select `eye colors` from species
+ */
 function parseQueryAndGenerateNodes(
   queryString: string,
   data: typeof SampleData
 ) {
-  // Determine tables to render
-  // Determine Aliases
-  // after FROM
-  // after JOIN
-  // console.log(queryString);
-  const nodes = [];
+  {
+    {
+      // const people: {
+      //   species_id: {
+      //     table_name: 'people';
+      //     column_name: 'species_id';
+      //     data_type: 'bigint';
+      //     foreign_key: true;
+      //     linkedTable: 'species';
+      //     linkedTableColumn: '_id';
+      //   };
+      //   birth_year: {
+      //     table_name: 'people';
+      //     column_name: 'birth_year';
+      //     data_type: 'varchar';
+      //   };
+      // };
+    }
+    // Determine tables to render
+    // Determine Aliases
+    // after FROM
+    // after JOIN
+    // console.log(queryString);
+    /*
+    SELECT col
+    FROM table
+    WHERE ...
 
-  const tablesToRender = [];
-  const columnsToHighlight = {};
-  const tableAlias: Record<string, string> = {};
-  const splitBySpace = queryString.split(/[(\s,|.)]+/);
+    SELECT col
+    FROM table (alias)
+    LEFT/RIGHT/INNER/OUTER/NOTHING JOIN table (alias) ON
 
-  for (let i = 0; i < splitBySpace.length; i++) {
-    const currentString = splitBySpace[i];
-    const previousString = splitBySpace[i - 1];
-    if (previousString === 'FROM' || previousString === 'JOIN') {
-      tablesToRender.push(currentString);
-      if (splitBySpace[i + 1] !== 'ON' || splitBySpace[i + 1] !== 'WHERE') {
-        tableAlias[splitBySpace[i + 1]] = currentString;
-        tableAlias[currentString] = splitBySpace[i + 1];
+    select people.name, p.hair_color, species.skin_colors from people p
+    left join species on people.species_id = species._id
+
+
+    main object:
+    {
+      films: [
+  `    {
+        table_name: 'films',
+        column_name: '_id',
+        data_type: 'int',
+      },
+      {
+        table_name: 'films',
+        column_name: 'title',
+        data_type: 'varchar',
+      },
+      {
+        table_name: 'films',
+        column_name: 'episode_id',
+        data_type: 'int',
+      },
+    ],
+    {
+      people:{
+        someColumnName: {
+          EVERY DATA WE NEED
+        }
+      }
+      films:{}
+    }
+    people:
+      {
+        _id: {
+          table_name: 'people',
+          column_name: '_id',
+          data_type: 'int',
+        }
+      }
+    ,
+      {
+        table_name: 'people',
+        column_name: 'name',
+        data_type: 'varchar',
+      },
+      {
+        table_name: 'people',
+        column_name: 'mass',
+        data_type: 'varchar',
+      },
+      {
+        table_name: 'people',
+        column_name: 'film_id',
+        data_type: 'bigint',
+        foreign_key: true,
+        linkedTable: 'films',
+        linkedTableColumn: '_id',
+      },
+      {
+        table_name: 'people',
+        column_name: 'homeworld_id',
+        data_type: 'bigint',
+        foreign_key: true,
+        linkedTable: 'planets',
+        linkedTableColumn: '_id',
+      },
+      {
+        table_name: 'people',
+        column_name: 'height',
+        data_type: 'int',
+      },
+    ],
+  },`
+
+  select p.name from people p
+  left join films f on p.film_id = f._id
+
+  tablesToRender = {
+    people: new Set([name])
+    films: new Set()
+  }
+
+  const smallerObject = {};
+  for (tableName of tablesToRender) {
+    smallerObject[tableName] = mainObject[tableName] // (object contains all columns)
+
+    for (colName of smallerObject[tableName]) {
+      if (tablesToRender[tableName].has(colName) {
+        smallerObject[tableName][colName].highlighted = true
+      }
+
+      if (smallerObject[colName].has(foreign_key)) {
+        newTable = smallerObject[colName].linkedTable
+
+        if (!tablesToRender.hasOwnProperty(newTable)) {
+          smallerObject[newTable] = mainObject[newTable]
+        }
       }
     }
   }
-  for (const table of tablesToRender) {
-    const tableObject: Record<string, any> = {};
-    tableObject.table_name = table;
-    tableObject.columns = [];
-    nodes.push(tableObject);
-  }
-  interface columnObj {
-    table_name: string;
-    column_name: string;
-    data_type: string;
-    primary_key?: boolean;
-    foreign_key?: boolean;
-    linkedTable?: string;
-    linkedTableColumn?: string;
-  }
 
-  /*
-  {
-    people: {
-      species_id: {
-        table_name: 'people',
-        column_name: 'species_id',
-        data_type: 'bigint',
-        foreign_key: true,
-        linkedTable: 'species',
-        linkedTableColumn: '_id',
-      },
-      birth_year: {
-        table_name: 'people',
-        column_name: 'birth_year',
-        data_type: 'varchar',
-      },
-    };
+  everything between SELECT and FROM is a colName
+  if colName has . array = colName.split('.')
+  tableAlias = array[0]
+  colName = array[1]
+
+  lookup table name in alias list, from there add cols to tables to render set
+
+    */
+    const nodes = [];
+    const keywords = new Set([
+      'LEFT',
+      'RIGHT',
+      'INNER',
+      'OUTER',
+      'ON',
+      'WHERE',
+      'JOIN',
+    ]);
+    const tablesToRender = [];
+    const columnsToHighlight = {};
+    const tableAlias: Record<string, string> = {};
+    const re = new RegExp(
+      /(?<=")([^"]*)(?=")|(?<=`)([^`]*)(?=`)|[^\s"`]+/,
+      'gm'
+    );
+    console.log(re);
+    const splitBySpace = queryString.split(re);
+
+    for (let i = 0; i < splitBySpace.length; i++) {
+      const currentString = splitBySpace[i];
+      const previousString = splitBySpace[i - 1];
+      const nextString = splitBySpace[i + 1];
+      if (previousString === 'FROM' || previousString === 'JOIN') {
+        tablesToRender.push(currentString);
+        if (!keywords.has(nextString)) tableAlias[nextString] = currentString;
+        else tableAlias[currentString] = currentString;
+      }
+    }
+
+    for (const table of tablesToRender) {
+      const tableObject: Record<string, any> = {};
+      tableObject.table_name = table;
+      tableObject.columns = [];
+      nodes.push(tableObject);
+    }
+    interface columnObj {
+      table_name: string;
+      column_name: string;
+      data_type: string;
+      primary_key?: boolean;
+      foreign_key?: boolean;
+      linkedTable?: string;
+      linkedTableColumn?: string;
+    }
+
+    /*
+
     tableName1: {},
     tableName2: {},
   }
   */
-  // interface innerObj {
-  //   string: columnObj;
-  // }
-  const nodesObj: Record<string, Record<string, columnObj>> = {};
-  for (const table of tablesToRender) nodesObj[table] = {};
-  console.log(splitBySpace);
-  // find columns in each table
-  if (Object.keys(tableAlias).length) {
-    for (let j = 0; j < splitBySpace.length; j++) {
-      const currEl = splitBySpace[j];
-      const prevEl = splitBySpace[j - 1];
-      if (currEl === 'FROM') break;
-      if (prevEl in tableAlias) {
-        if (currEl === '*') {
-          for (let k = 0; k < SampleData.length; k++) {
-            if (SampleData[k].table_name in nodesObj) {
-              for (let l = 0; l < SampleData[k].columns.length; l++) {
-                if (nodes[l].table_name in nodesObj)
-                  nodes[l].columns = [...SampleData[k].columns];
+    // interface innerObj {
+    //   string: columnObj;
+    // }
+    const nodesObj: Record<string, Record<string, columnObj>> = {};
+    for (const table of tablesToRender) nodesObj[table] = {};
+    console.log(splitBySpace);
+    // find columns in each table
+    if (Object.keys(tableAlias).length) {
+      for (let j = 0; j < splitBySpace.length; j++) {
+        const currEl = splitBySpace[j];
+        const prevEl = splitBySpace[j - 1];
+        if (currEl === 'FROM') break;
+        if (prevEl in tableAlias) {
+          if (currEl === '*') {
+            for (let k = 0; k < SampleData.length; k++) {
+              if (SampleData[k].table_name in nodesObj) {
+                for (let l = 0; l < SampleData[k].columns.length; l++) {
+                  if (nodes[l].table_name in nodesObj)
+                    nodes[l].columns = [...SampleData[k].columns];
+                  break;
+                }
                 break;
               }
-              break;
             }
           }
+          // currEl is the column
         }
-        // currEl is the column
+      }
+    } else {
+      // There are no aliases
+      let start;
+      let end;
+      let tableName: string;
+      for (let i = 0; i < splitBySpace.length; i++) {
+        const currentString = splitBySpace[i];
+        if (currentString === 'SELECT') start = i + 1;
+        if (currentString === 'FROM') {
+          end = i - 1;
+          tableName = splitBySpace[i + 1];
+        }
+      }
+      const columnsRendered = splitBySpace.slice(start, end);
+      for (let i = 0; i < columnsRendered.length; i++) {
+        // nodesObj[tableName][columnsRendered[i]] =
+        //    {
+        //     table_name: tableName,
+        //     column_name: columnsRendered[i],
+        //   },
       }
     }
-  } else {
-    // There are no aliases
-    let start;
-    let end;
-    let tableName: string;
-    for (let i = 0; i < splitBySpace.length; i++) {
-      const currentString = splitBySpace[i];
-      if (currentString === 'SELECT') start = i + 1;
-      if (currentString === 'FROM') {
-        end = i - 1;
-        tableName = splitBySpace[i + 1];
-      }
-    }
-    const columnsRendered = splitBySpace.slice(start, end);
-    for (let i = 0; i < columnsRendered.length; i++) {
-      // nodesObj[tableName][columnsRendered[i]] =
-      //    {
-      //     table_name: tableName,
-      //     column_name: columnsRendered[i],
-      //   },
-    }
-  }
 
-  console.log('alias', tableAlias);
-  console.log('tables', tablesToRender);
+    console.log('alias', tableAlias);
+    console.log('tables', tablesToRender);
+  }
 }
 parseQueryAndGenerateNodes(str, SampleData);
 //notes on parsing
