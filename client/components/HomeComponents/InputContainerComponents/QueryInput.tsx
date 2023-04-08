96fc43e48456984567131965b89ba8b1297e605b
melissamcl <mcl.melissa.m@gmail.com>
2023-04-08 00:16:02 -0400

    fixed error messaging placement/styling

--- client/components/HomeComponents/InputContainerComponents/QueryInput.tsx
+++ client/components/HomeComponents/InputContainerComponents/QueryInput.tsx
@@ -1,4 +1,10 @@
-import React, { FC, useState, useContext, useEffect } from 'react';
+import React, {
+  FC,
+  useState,
+  useContext,
+  useEffect,
+  CSSProperties,
+} from 'react';
 import { HomepageContext } from '../../../Context';
 import { historyType } from '../../../Context';
 import { parse, Statement, astVisitor } from 'pgsql-ast-parser';
@@ -88,7 +94,7 @@ const QueryInput: React.FC<{}> = () => {
   }, 1000);
 
   return (
-    <div className="query-main" style={{ height: 100 % -5 }}>
+    <div className="query-main">
       <div className="query-main-inner">
         <textarea
           className="query-input"
@@ -98,9 +104,6 @@ const QueryInput: React.FC<{}> = () => {
           value={queryString}
           onKeyUp={handlePause}
         ></textarea>
-        {errorMessages[0] && (
-          <div className="error-message-container">{err}</div>
-        )}
         <button
           type="submit"
           className="submit-query-button"
@@ -109,6 +112,7 @@ const QueryInput: React.FC<{}> = () => {
           →
         </button>
       </div>
+      {errorMessages[0] && <div className="error-message-container">{err}</div>}
     </div>
   );
 };
